631bf1ac33048d7688fa2a769b231b5e6d2725d1
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-22 17:21:17 -0500

    Tested Custom csrfFetch with CSRF

---
+++ frontend/src/index.js
@@ -0,0 +1,47 @@
+// frontend/src/index.js
+import React from 'react';
+
+import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+
+
+// frontend/src/index.js
+// ...
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+// frontend/src/index.js
+// ...
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
