ca9c5051b61a1bba1a0aa5d600b30bb59ae645ae
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-22 12:50:26 -0500

    Phase 4fourrr

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,69 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 module.exports = router;
+
+// // backend/routes/api/index.js
+// const router = require('express').Router();
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+// // backend/routes/api/index.js
+// // ...
+
+// // GET /api/set-token-cookie
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // ...
+
+// // backend/routes/api/index.js
+// // ...
+
+// // GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     console.log(req.user);
+//     return res.json(req.user);
+//   }
+// );
+
+// // ...
+
+// // backend/routes/api/index.js
+// // ...
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // ...
+
+// module.exports = router;
