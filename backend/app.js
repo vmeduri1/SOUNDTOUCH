40ffbea09eb6a347ba582ea5da8312c1928fb64d
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-22 11:35:38 -0500

    Finished Phase 2

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 
 app.use(morgan('dev'));
@@ -36,6 +37,35 @@ if (!isProduction) {
     })
   );
 
-  app.use(routes); // Connect all the routes
+app.use(routes); // Connect all the routes
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
