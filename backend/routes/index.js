54b3c95fbdce64b04b5326ac4eb58b413f40dc3a
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-22 11:23:27 -0500

    Finished Phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,15 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
 });
+
+
 
 module.exports = router;
