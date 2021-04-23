e03113acfc3fe0276d3994052574433ccec0b45e
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-04-23 16:31:07 -0500

    Finished auth

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -71,4 +71,12 @@ export const restoreUser = () => async dispatch => {
     return response;
 };
 
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    })
+    dispatch(removeUser());
+    return response;
+};
+
 export default sessionReducer;
