d4536a29127f001667b0287171ceb80da1091789
Vishaal Meduri <vishaal@Vishaals-MBP.attlocal.net>
2022-04-22 09:31:08 -0500

    Set Heroku Interval Timer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,12 @@ function App() {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
+  // taps heroku app every 5 minutes
+  var http = require("http");
+  setInterval(function() {
+    http.get("http://https://sound-touch.herokuapp.com/");
+  }, 300000); // every 5 minutes (300000)
+
   return (
     <>
       <Navigation isLoaded={isLoaded} />
